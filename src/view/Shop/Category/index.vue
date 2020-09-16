89f87984b8bd22368948187bd516cbd65a69bebb
sxt <929706033@qq.com>
2020-09-16 09:16:07 +0800

    写好的代码备份

--- src/view/Shop/Category/index.vue
+++ src/view/Shop/Category/index.vue
@@ -1,45 +1,17 @@
 <template>
  <div>
-   <!-- <el-button type="primary"  @click="add"> 添加菜单</el-button> -->
-   <el-button type="primary" @click="add">添加类别</el-button>
-
-    <v-list @edit="edit"></v-list>
-    <v-info :info="info" ref="dialog"></v-info>
+分类 
 </div>
 </template>
 
 <script>
-import VList from "./list" 
-import VInfo from "./vinfo" 
  export default {
    data () {
-     return {
-       info:{
-        //  dialogVisible:false,
-         isAdd:false,
-         isShow:false
-       }
-     }
+     return {}
    },
    created() {},
-   methods: {
-     change(){
-       this.info.dialogVisible = true;
-     },
-     add(){
-       this.info.isAdd = this.info.isShow = true; 
-       console.log("哈哈")
-     },
-     edit(val){
-        this.info.isAdd = false;
-        this.info.isShow = true;
-        // 调用弹框组件的setinfo方法！
-        this.$refs.dialog.setinfo(val);
-      }
-   },
-   components: {
-     VList,VInfo
-   }
+   methods: {},
+   components: {}
  }
 </script>
 
